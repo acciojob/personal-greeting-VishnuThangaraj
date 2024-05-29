77c0f45acc3475399df4ce0714687749e103da63
VishnuThangaraj <vishnuthangaraj.private@gmail.com>
2024-05-29 20:58:33 +0530

    f

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,12 @@ import React from "react";
 import "./../styles/App.css";
 
 const App = () => {
-  const [name, setName] = useState(null);
+  const [name, setName] = useState("");
 
   const onChangeEvent = (event) => {
-    setName(event.target.value);
+    setName(`Hello ${event.target.value}!`);
   };
+
   return (
     <div>
       {
